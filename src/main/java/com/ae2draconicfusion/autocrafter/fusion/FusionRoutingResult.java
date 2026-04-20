45e96e5a35bbdf86a3bcd70f1ea7673f378d2d13
Fagghino <fgiordano839@gmail.com>
2026-04-20 15:36:44 +0200

    feat(fusion): add routing result enumeration

---
+++ src/main/java/com/ae2draconicfusion/autocrafter/fusion/FusionRoutingResult.java
@@ -0,0 +1,10 @@
+package com.ae2draconicfusion.autocrafter.fusion;
+
+public enum FusionRoutingResult {
+    SUCCESS,
+    INVALID_CORE,
+    NO_INJECTORS_FOUND,
+    INJECTORS_INSUFFICIENT,
+    TARGET_SLOTS_OCCUPIED,
+    TEMPORARY_FAILURE
+}
