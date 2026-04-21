95204e41b204c170de5e3bd491023a131ce28ef9
Fagghino <fgiordano839@gmail.com>
2026-04-21 20:08:34 +0200

    feat: add clone item stack method to DraconicPatternProviderBlock

--- src/main/java/com/ae2draconicfusion/autocrafter/ae2/DraconicPatternProviderBlock.java
+++ src/main/java/com/ae2draconicfusion/autocrafter/ae2/DraconicPatternProviderBlock.java
@@ -2,10 +2,22 @@ package com.ae2draconicfusion.autocrafter.ae2;
 
 import appeng.block.crafting.PatternProviderBlock;
 import appeng.blockentity.crafting.PatternProviderBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.HitResult;
+import com.ae2draconicfusion.autocrafter.registry.ModItems;
 
 public final class DraconicPatternProviderBlock extends PatternProviderBlock {
     public void bindBlockEntityType(BlockEntityType<PatternProviderBlockEntity> blockEntityType) {
         setBlockEntity(PatternProviderBlockEntity.class, blockEntityType, null, null);
+    }
+
+    @Override
+    public ItemStack getCloneItemStack(LevelReader level, BlockPos pos, BlockState state) {
+        return new ItemStack(ModItems.ME_DRACONIC_PATTERN_PROVIDER.get());
     }
 }
